a525b77914490335d185eb88d031725ac937e765
Andrew McGill <arm5077@gmail.com>
2016-10-17 08:55:03 -0400

    Actually turn on server this time

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ app.get('/getMessage', function(req, res){
   console.log("blooooo");
   res.send("Nailed it!")
 });
+
+app.listen(process.env.port || 3000, function(){
+  console.log("listening!")
+})
