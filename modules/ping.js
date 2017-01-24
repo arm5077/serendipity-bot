d747b74a9235fec703204a10fcac2597d520571c
Andrew McGill <arm5077@gmail.com>
2017-01-24 13:02:53 -0500

    second attempt at squashing bug

--- modules/ping.js
+++ modules/ping.js
@@ -59,21 +59,18 @@ module.exports = function(){
             var person = potentials[index];
             
             // See if this person is active
-            return slack.api("users.list", { "presence": "active" }, function(err, data){
+            slack.api("users.list", { "presence": "active" }, function(err, data){
               if(err) throw err;
               var activeUsers = data.members.map(function(d){ return d.name });
               if( activeUsers.indexOf(person.username) == -1){
                 console.log("Was going to try " + person.username + ", but they're not active")
-                return
               }
-                
-              bot.postMessageToUser(person.username, "Hi! I'm SerendipityBot, created by <@U0EKQHS6L|andrewmcgill>. I ask random Atlantic people what they're working on this week that excites them, and share a brief digest every day in <#C2RC1STC2|serendipity>. It's a fun way to get an idea of the cool things your coworkers are doing.\n\nWant to partipate? If so, tell me something you're working on this week that you'd like to share, in the form of \"I am *building a rocket,*\" or \"I'm *writing about Capt. Jean-Luc Picard*.\"\n\nIf you don't want me to bug you right now, reply *\"snooze.\"* If you never want to hear from me again (:sob:), type *\"I hate you.\"*", { 'slackbot': false, icon_emoji: ':thinking_face:' })
-              console.log("bugging " + person.username)
-              collection.update({ username: person.username }, { $set: { status: "touched base, waiting for response" }});
-              
+              else {
+                bot.postMessageToUser(person.username, "Hi! I'm SerendipityBot, created by <@U0EKQHS6L|andrewmcgill>. I ask random Atlantic people what they're working on this week that excites them, and share a brief digest every day in <#C2RC1STC2|serendipity>. It's a fun way to get an idea of the cool things your coworkers are doing.\n\nWant to partipate? If so, tell me something you're working on this week that you'd like to share, in the form of \"I am *building a rocket,*\" or \"I'm *writing about Capt. Jean-Luc Picard*.\"\n\nIf you don't want me to bug you right now, reply *\"snooze.\"* If you never want to hear from me again (:sob:), type *\"I hate you.\"*", { 'slackbot': false, icon_emoji: ':thinking_face:' })
+                console.log("bugging " + person.username)
+                collection.update({ username: person.username }, { $set: { status: "touched base, waiting for response" }});
+              }  
             })
-        
-            
           })
         }) 
       } else {
